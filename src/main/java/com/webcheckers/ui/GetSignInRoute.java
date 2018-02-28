e07ce80239752f1e37d5740835d986606415a75d
Joel Margolis <joel@silocode.net>
2018-02-28 16:25:28 -0500

    Re-add Brandon's code

---
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -0,0 +1,60 @@
+package com.webcheckers.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+
+/**
+ * The UI Controller to GET the Home page.
+ *
+ * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+ */
+public class  GetSignInRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
+
+    private final TemplateEngine templateEngine;
+
+    /**
+     * Create the Spark Route (UI controller) for the
+     * {@code GET /} HTTP request.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
+    public GetSignInRoute (final TemplateEngine templateEngine) {
+        // validation
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        //
+        this.templateEngine = templateEngine;
+        //
+        LOG.config("GetSignInRoute is initialized.");
+    }
+
+    /**
+     * Render the WebCheckers Home page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Home page
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetSignInRoute is invoked.");
+        //
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome!");
+        return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
+    }
+
+}
