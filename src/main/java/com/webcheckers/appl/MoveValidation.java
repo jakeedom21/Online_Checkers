0524a6af35340fb92a6af8c9e7598e4e71d5737a
Jake <jakeedom21@gmail.com>
2018-03-03 13:19:50 -0500

    Additional comments on basicMove

--- src/main/java/com/webcheckers/appl/MoveValidation.java
+++ src/main/java/com/webcheckers/appl/MoveValidation.java
@@ -8,12 +8,28 @@ import java.util.ArrayList;
 
 /**
  * Created by Jake on 3/2/2018.
+ * basicMoves:
+ * Will get all possible single space moves from a piece does
+ * only validation it does is that it doesn't go out of bounds
+ *
+ * Will probably use this to help pathfind to the final space
+ *
+ * For validation
+ *
+ * Current problems:
+ * Finding the path in such a way it could be used for Player Help
+ * Determining path when there are multiple valid ways to a space
+ * Best way may be when it detects multiple valid paths to have the player do it one jump at a time
+ *
+ * Pathfinder 
  */
 public class MoveValidation {
     private final static int MAX_DIM = 8;
 
     /**
-     * Get the basic moves for Red pieces
+     * Get the basic moves for all pieces
+     * this means the all moves from the single space/piece one space away
+     * will see if it is a valid move later
      * @param piece
      * @return
      */
@@ -59,6 +75,7 @@ public class MoveValidation {
         return basicMoves;
     }
 
+    public ArrayList<Space> findPath(Piece movingPiece, )
 
     public boolean validMove(Piece movingPiece, Space finalSpace, Board gameBoard){
         //if both col and row are even or odd is an invalid move
@@ -82,7 +99,6 @@ public class MoveValidation {
         }
         //finalSpace is a valid place for a pieces to be
         //will need to no check if it can jump over pieces to finalSpace
-        return false;
 
     }
 }
