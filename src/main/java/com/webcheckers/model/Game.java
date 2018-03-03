56627379a329e5d89581377e0362bf1bf9339a70
Jake <jakeedom21@gmail.com>
2018-03-02 23:35:20 -0500

    Created and partially implemented MoveValidation

--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -4,20 +4,20 @@ package com.webcheckers.model;
  * Created by qadirhaqq on 2/28/18.
  */
 public class Game {
-    private final User p1;
-    private final User p2;
+    private final Player p1;
+    private final Player p2;
     private Board board;
-    private User playerTurn;
+    private Player playerTurn;
     private boolean forfeit = false;
 
-    public Game(Integer id, User p1, User p2) {
+    public Game(Integer id, Player p1, Player p2) {
         this.p1 = p1;
         this.p2 = p2;
         this.board = new Board();
     }
 
-    public User[] getPlayers() {
-        User[] players = new User[2];
+    public Player[] getPlayers() {
+        Player[] players = new Player[2];
         players[0] = p1;
         players[1] = p2;
         return players;
