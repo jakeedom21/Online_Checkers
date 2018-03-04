38334619ccdb54700895e8df6b99d3eedc6a66aa
Qadir Haqq <Haqq.Q173@gmail.com>
2018-03-04 00:23:54 -0500

    Game screen renders

--- src/main/java/com/webcheckers/appl/RouteManager.java
+++ src/main/java/com/webcheckers/appl/RouteManager.java
@@ -4,7 +4,7 @@ package com.webcheckers.appl;
  * Created by qadirhaqq on 2/27/18.
  */
 
-import com.webcheckers.controller.GameController;
+import com.webcheckers.ui.GetGameRoute;
 import spark.Spark;
 
 public class RouteManager {
@@ -13,18 +13,17 @@ public class RouteManager {
     public static final String SIGNIN_ROUTE = "/signin";
     public static final String SPECFIC_GAMES_ROUTE = GAMES_ROUTE + "/:id";
 
-    public static GameController gameController;
+    public static GetGameRoute getGameRoute;
     public static PlayerLobby playerLobby;
 
-    public RouteManager(GameController gameController, PlayerLobby playerLobby) {
-        this.gameController = gameController;
+    public RouteManager(GetGameRoute getGameRoute, PlayerLobby playerLobby) {
+        this.getGameRoute = getGameRoute;
         this.playerLobby = playerLobby;
 
     }
 
     public void init() {
-        Spark.post(GAMES_ROUTE, gameController::createNewGame);
-        Spark.get(SPECFIC_GAMES_ROUTE, gameController::renderGamePage);
+        Spark.post(GAMES_ROUTE, getGameRoute::renderGamePage);
     }
 
 }
