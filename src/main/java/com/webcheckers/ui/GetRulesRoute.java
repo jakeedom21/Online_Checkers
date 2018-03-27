0c267061e48d69acf9e22ebd2e38574b1129aff3
Joel Margolis <joel@teknogeek.io>
2018-03-27 19:41:09 -0400

    Add rules page

---
+++ src/main/java/com/webcheckers/ui/GetRulesRoute.java
@@ -0,0 +1,48 @@
+package com.webcheckers.ui;
+
+
+import java.util.HashMap;
+import java.util.Objects;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+
+/**
+ * The UI Controller to GET the Rules page.
+ */
+public class GetRulesRoute implements Route {
+    private static final String VIEW_NAME = "rules.ftl";
+    private final TemplateEngine templateEngine;
+
+    /**
+     * Create the Spark Route (UI controller) for the
+     * {@code GET /} HTTP request.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
+    public GetRulesRoute(final TemplateEngine templateEngine) {
+        // validation
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        this.templateEngine = templateEngine;
+    }
+
+    /**
+     * Render the WebCheckers Home page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Rules page
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        return templateEngine.render(new ModelAndView(new HashMap<>(), VIEW_NAME));
+    }
+
+}
