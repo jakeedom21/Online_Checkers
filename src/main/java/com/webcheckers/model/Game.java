02a5a018b5fe077a7cae6b8030c9d455ec54f0fe
Jake <jakeedom21@gmail.com>
2018-03-30 10:48:23 -0400

    Successfully removes pieces when jumped

--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -148,31 +148,5 @@ public class Game {
 
         }
     }
-
-    /**
-     * Takes in a space and destroy's the piece at that space
-     * Takes the space and which player is moving and destroys the piece on their board
-     * Then takes the board and flips it and makes that the other player's board
-     * @param removePiece
-     *        playerMoving
-     */
-    public void removePiece(Space removePiece, Player playerMoving){
-        int row = removePiece.getRow();
-        int col = removePiece.getCol();
-        //destroy's piece in p1Board and replaces the p2Board
-        if(playerMoving.equals(this.p1)){
-            p1Board.getSpace(row, col).setPiece(null);
-            Board newp1Board = new Board(p1Board);
-            newp1Board.flip();
-            p2Board = newp1Board;
-        }
-        //destroy's piece in p2Board and replaces the p1Board
-        else{
-            this.p2Board.getSpace(row, col).setPiece(null);
-            Board newp2Board = new Board(p2Board);
-            newp2Board.flip();
-            this.p1Board = newp2Board;
-        }
-    }
 }
 
