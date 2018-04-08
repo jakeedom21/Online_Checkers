6449586ebc58a1a78974f0550be3980f1b89a856
Qadir Haqq <Haqq.Q173@gmail.com>
2018-04-08 13:54:24 -0400

    Deleted extraneous spacing

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -16,8 +16,7 @@ public class Board implements Serializable{
     private static final String PLAYER2_COLOR = "R";
     private static HashSet<Integer> EMPTY_ROWS = new HashSet<>();
     private Space[][] board = new Space[MAX_DIM][MAX_DIM];
-
-
+    
     /**
      * Constructor for board object
      * //Picture used as reference: http://allaboutfunandgames.com/wp-content/uploads/2011/11/Checkers.jpg
